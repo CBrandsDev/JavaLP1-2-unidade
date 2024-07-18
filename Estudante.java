cfd355b4ddf167d2581f03f4a170c15968531b2f
CBrandsDev <cbrandao2001@gmail.com>
2024-07-18 11:00:16 -0300

    class extends

---
+++ Estudante.java
@@ -0,0 +1,20 @@
+public class Estudante extends Socio {
+
+    private String universidade;
+
+    public Estudante(String n, String c, double vA, int i, String u) {
+        super(n, c, vA, i);
+        universidade = u;
+    }
+
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+    public String getUniversidade() {
+        return universidade;
+    }
+
+    public void imprimir() {
+        System.out.println("O Estudante " +super.nome+ "de " +super.idade+ " anos, estudante da universidade " + universidade);
+    }
+}
